d059ee5ddabf184126bbaf8815597d73954d2b08
Felipe <felipecarvalho180@gmail.com>
2019-04-28 07:06:46 -0300

    Updates

---
+++ precache-manifest.2cfb5b85217aedf2c14761827afcd365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d556e8a0ae185f61cf33",
+    "url": "/static/js/main.d556e8a0.chunk.js"
+  },
+  {
+    "revision": "c9b24f14d3ecca3710d9",
+    "url": "/static/js/2.c9b24f14.chunk.js"
+  },
+  {
+    "revision": "d556e8a0ae185f61cf33",
+    "url": "/static/css/main.e94c4a1c.chunk.css"
+  },
+  {
+    "revision": "0372f4b6e89599ad3800711c6c76baf0",
+    "url": "/index.html"
+  }
+];
