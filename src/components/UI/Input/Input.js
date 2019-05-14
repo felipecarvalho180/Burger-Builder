65fa78197929cd01c6bed4a4225fb08b0d056d7a
Felipe <felipecarvalho180@gmail.com>
2019-05-14 19:24:05 -0300

    .

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,12 +3,17 @@ import classes from './Input.css'
 
 const input = (props) => {
     let inputElement = null;
+    let inputClasses = [classes.InputElement];
+
+    if(props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid)
+    }
 
     switch (props.elementType) {
         case ( 'input' ):
             inputElement = 
                 <input 
-                    className={classes.InputElement} 
+                    className={inputClasses.join(' ')} 
                     {...props.elementConfig}
                     value={props.value}
                     onChange={props.changed}/>;
@@ -17,7 +22,7 @@ const input = (props) => {
         case ( 'textarea' ):
             inputElement = 
                 <textarea 
-                    className={classes.InputElement} 
+                    className={inputClasses.join(' ')} 
                     {...props.elementConfig}
                     value={props.value}
                     onChange={props.changed}/>;
@@ -26,7 +31,7 @@ const input = (props) => {
         case ( 'select' ):
             inputElement = (
                 <select 
-                    className={classes.InputElement} 
+                    className={inputClasses.join(' ')} 
                     value={props.value}
                     onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
@@ -41,7 +46,7 @@ const input = (props) => {
         default:
             inputElement = 
                 <input 
-                    className={classes.InputElement} 
+                    className={inputClasses.join(' ')} 
                     {...props.elementConfig}
                     value={props.value}/>;
             break;
