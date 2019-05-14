f5022dd6a5494144e8d90f3c44809086d4ff7645
Felipe <felipecarvalho180@gmail.com>
2019-05-14 19:25:08 -0300

    Updates

---
+++ precache-manifest.2726086eb3527f371fa6810c9dd5b4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "2573fa13af0a41c50216",
+    "url": "/Burger-Builder/static/js/runtime~main.2573fa13.js"
+  },
+  {
+    "revision": "f21ee48e0bc41e29981e",
+    "url": "/Burger-Builder/static/js/main.f21ee48e.chunk.js"
+  },
+  {
+    "revision": "6310a447de4ee73d429b",
+    "url": "/Burger-Builder/static/js/2.6310a447.chunk.js"
+  },
+  {
+    "revision": "f21ee48e0bc41e29981e",
+    "url": "/Burger-Builder/static/css/main.265d8264.chunk.css"
+  },
+  {
+    "revision": "e884b21c65f88cd7c85d118995708365",
+    "url": "/Burger-Builder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Burger-Builder/precache-manifest.782a51c19aee6377ac23be69d440670d.js"
+  "/Burger-Builder/precache-manifest.2726086eb3527f371fa6810c9dd5b4e0.js"
 );
 
 workbox.clientsClaim();
