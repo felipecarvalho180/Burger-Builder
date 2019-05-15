1908b8068e8f1f35789df7d52bf31fb4e033e4f6
Felipe <felipecarvalho180@gmail.com>
2019-05-15 20:25:15 -0300

    fc

--- src/containers/CheckOut/ContactData/ContactData.js
+++ src/containers/CheckOut/ContactData/ContactData.js
@@ -18,7 +18,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -30,7 +31,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             zipcode: {
                 elementType: 'input',
@@ -44,7 +46,8 @@ class ContactData extends Component {
                     minLength: 5,
                     maxLength: 5
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -56,7 +59,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -68,7 +72,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -78,10 +83,13 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}
                     ]
                 },
-                value: ''
+                value: '',
+                valid: true,
+                validation: {}
             }
         },
-        loading: false
+        loading: false,
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -108,11 +116,11 @@ class ContactData extends Component {
         console.log(this.props.price);
     }
 
-    checkValidity(value, rules ) {
-        let isValid = true
+    checkValidity(value, rules) {
+        let isValid = true;
 
         if (rules.required) {
-            isValid = value.trim() !== '' && isValid
+            isValid = value.trim() !== ' ' && isValid
         }
 
         if (rules.minLength) {
@@ -135,9 +143,15 @@ class ContactData extends Component {
         };
         updateFormElement.value = event.target.value;
         updateFormElement.valid = this.checkValidity(updateFormElement.value, updateFormElement.validation);
+        updateFormElement.touched = true;
         updateOrderForm[inputIndentifier] = updateFormElement;
-        console.log(updateFormElement)
-        this.setState({orderForm: updateOrderForm});
+
+        let formIsValid = true;
+        for (let inputIndentifier in updateOrderForm) {
+            formIsValid = updateOrderForm[inputIndentifier].valid && formIsValid
+        }
+
+        this.setState({orderForm: updateOrderForm, formIsValid: formIsValid});
     }
 
     render(){
@@ -158,11 +172,12 @@ class ContactData extends Component {
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
                         invalid={!formElement.config.valid}
+                        touched={formElement.config.touched}
                         shouldValidate={formElement.config.validation}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}             
                 <Button 
-                    btnType="Success">ORDER</Button>
+                    btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         );
         if (this.state.loading) {
